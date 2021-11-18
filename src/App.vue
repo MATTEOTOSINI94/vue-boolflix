21aa996133a4b8593160a53a1460ca73b6233708
MATTEOTOSINI94 <matteotosini300@icloud.com>
2021-11-18 09:42:26 +0100

    seconda milestone

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,82 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <input type="text"  v-model="filterBar" @keyup.enter="searchAll">
+    <ul>
+      <h1>FILM..</h1>
+      <li v-for="(movie) in movies" :key="movie.id">
+        {{movie.title}} ({{movie.original_title}}) <img :src="flags[movie.original_language] || flags.default" alt=""> {{movie.original_language}} <br>
+        {{movie.vote_average}}
+      </li>
+        <h1>SERIE TV..</h1>
+      <li v-for="(series) in series" :key="series.id">
+      {{series.name}} ({{ series.original_name}}) 
+      <img :src="flags[series.original_language] || flags.default" alt="">
+      {{series.original_language}} <br>
+      {{series.vote_average}}
+      </li>
+
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import axios from 'axios'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+ data(){
+   return{
+     keyApi:"c1f56e9f69a038bc79759d802d8810c2",
+     urlApi:"https://api.themoviedb.org/3",
+     filterBar:"",
+     movies:[],
+     series:[],
+     flags:{
+       en:require("./assets/en.png"),
+       fr:require("./assets/fr.svg"),
+       ja:require("./assets/jap.png"),
+       es:require("./assets/sp.jpg"),
+       default:require("./assets/default.png"),
+     }
+   }
+  },
+
+  methods:{
+
+    searchAll(){
+      if (this.filterBar === "") {
+          return this.movies  = [],
+                  this.series = []
+      }
+        this.filmCall("/search/movie","movies")
+        this.filmCall("/search/tv","series")
+    },
+
+    filmCall(tipologia ,contenitore){ axios.get(this.urlApi + tipologia,{
+       params:{
+         api_key: this.keyApi,
+         query: this.filterBar
+       }
+     }).then(lista => {
+       this[contenitore] = lista.data.results
+     })},
+  },
+
+
+   mounted(){
+   
+ }
 }
 </script>
 
 <style lang="scss">
+@import "~bootstrap/scss/bootstrap";
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  h1{color: black;}
+  img{
+    width: 100px;
+  }
 }
 </style>
